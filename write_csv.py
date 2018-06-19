c2f6061b396c7e2bf99027c9238b19cd29af0d7c
Kose-i <tamura.kosei905@mail.kyutech.jp>
2018-06-19 17:00:55 +0900

    python forcsv file

---
+++ write_csv.py
@@ -0,0 +1,10 @@
+import csv
+ 
+mojiretu = "Hello World first csv file"
+data = mojiretu.split()
+print(data)
+
+f = open('result.csv', 'w')
+writer = csv.writer(f)
+writer.writerows(data)
+f.close()
