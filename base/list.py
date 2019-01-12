93d82350acccd74253950015a66afab4f34a47f5
Kose-i <tamura.kosei905@mail.kyutech.jp>
2019-01-12 15:47:55 +0900

    Add Duplit list

--- base/list.py
+++ base/list.py
@@ -1,8 +1,17 @@
 x = [1,2,3,4,5]
 
 print(type(x))
-
 print(x)
 print(x[2:])
 print(x[2:4])
 print(x[-2])
+
+y = [[0] * 3] * 4
+
+print(y)
+y[1][2] = 3
+print(y)
+
+z = [[i+j for i in range(1+j,3+j)] for j in range(4)]
+
+print(z)
