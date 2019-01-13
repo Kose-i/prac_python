233b900fe0a0e87e46e6f98ca261da2dba01cc4d
Kose-i <tamura.kosei905@mail.kyutech.jp>
2019-01-13 13:17:41 +0900

    Add tensorflow dir

---
+++ tensorflow/base.py
@@ -0,0 +1,12 @@
+import tensorflow as tf
+
+sess = tf.Session()
+
+a = tf.constant([1,2])
+print(sess.run(a))
+
+b = tf.constant([1,2,3])
+print(sess.run(b))
+
+c = tf.constant([[1,2,3][4,5,6]])
+print(sess.run(c))
