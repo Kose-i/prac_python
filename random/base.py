7c87fae8cffc356f77ade22351c528c9798009ef
Kose-i <tamura.kosei905@mail.kyutech.jp>
2019-01-30 16:20:42 +0900

    Add func randint

--- random/base.py
+++ random/base.py
@@ -7,4 +7,9 @@ for i in range(3):
   x.append(random.random())
 print(x) # 0, 1
 
+y = []
+for i in range(3):
+  y.append(random.randint(0,1))
+print(y)
+
 print(collections.Counter([math.floor(i*10) for i in x]))
