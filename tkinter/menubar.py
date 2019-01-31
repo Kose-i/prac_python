39c9435540dcf3c90531bc9c986a5ff4ed5f6184
Kose-i <tamura.kosei905@mail.kyutech.jp>
2019-01-31 10:08:16 +0900

    Add menubar target

--- tkinter/menubar.py
+++ tkinter/menubar.py
@@ -2,18 +2,35 @@
 
 import tkinter as tk
 
-def callback():
-  print('a')
 
 root = tk.Tk()
 root.geometry('800x460')
 root.title('menubar')
 
+def finish_callback():
+  root.destroy()
+
 menubar = tk.Menu(root)
 root.config(menu = menubar)
 
-filemenu = tk.Menu(menubar)
-menubar.add_cascade(label='ファイル', menu=filemenu)
-filemenu.add_command(label='閉じる', command=callback)
+cmdmenu = tk.Menu(menubar)
+menubar.add_cascade(label='コマンドメニュー', menu=cmdmenu)
+cmdmenu.add_command(label='閉じる', command=finish_callback)
+
+checkmenu = tk.Menu(menubar)
+menubar.add_cascade(label='チェックメニュー', menu=checkmenu)
+checkmenu.add_checkbutton(label='test1')
+
+radiomenu = tk.Menu(menubar)
+menubar.add_cascade(label='ラジオメニュー', menu=radiomenu)
+radiomenu.add_radiobutton(
+  label='test1',
+  variable = tk.IntVar(),
+  value = 0
+)
+
+separatormenu = tk.Menu(menubar)
+menubar.add_cascade(label='区切り線', menu=separatormenu)
+separatormenu.add_separator()
 
 root.mainloop()
