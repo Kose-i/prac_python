3aa8aac6555ed98ed741021078edcaa206f61abc
Kose-i <tamura.kosei905@mail.kyutech.jp>
2019-02-19 13:43:48 +0900

    Add try-except-finally

--- base/try-except.py
+++ base/try-except.py
@@ -1,1 +1,20 @@
 #! /usr/bin/env python3
+
+def error_happen():
+  try:
+    print("In try")
+    raise ValueError("raise ValueError")
+  except TypeError:
+    print("except 1")
+  except ValueError as Instance:
+    print("except 2")
+    print(Instance)
+  except:
+    print("other except")
+  else:
+    print("pass error")
+  finally:
+    print("finally")
+
+if __name__=='__main__':
+  error_happen()
