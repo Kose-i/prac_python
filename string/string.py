424671a8868a8764a84b5a7202717b1a9c98d020
Kose-i <tamura.kosei905@mail.kyutech.jp>
2019-02-19 09:57:15 +0900

    Add string

--- string/string.py
+++ string/string.py
@@ -2,13 +2,21 @@
 
 def func1():
   str = "%s is %s."%('aiueo', 'kakikukeko')
+  str1 = "tmpstr"
+
   print(str)
+  print("str.find('k')", str.find('k'))
+  print(','.join([str, str1]))
+  print(str.startswith('a'))
+  print(str.split('k'))
 
 def func2():
-  pass
+  str = "hogehogehoge"
+  print(str.replace('o','a'))
+  print(str.strip('h'))
 
 if __name__=='__main__':
-  print("func1()")
+  print("\nfunc1()")
   func1()
-  print("func2()")
+  print("\nfunc2()")
   func2()
