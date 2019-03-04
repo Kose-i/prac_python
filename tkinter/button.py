0b245c961c0c0dd677c4703081f25d83cfad6cdc
Kose-i <tamura.kosei905@mail.kyutech.jp>
2019-03-04 09:50:23 +0900

    Add button change command opt

--- tkinter/button.py
+++ tkinter/button.py
@@ -6,4 +6,26 @@ root = tk.Tk()
 root.geometry('800x460')
 root.title('button')
 
+frame = tk.Frame(
+    root,
+    relief=tk.RIDGE,
+    borderwidth=4
+)
+
+button_text_variable = tk.StringVar()
+def update_button_text():
+    button_text_variable.set("b")
+
+button = tk.Button(
+    frame,
+#    width=15,
+    textvariable=button_text_variable,
+    command=update_button_text
+)
+
+button_text_variable.set("a")
+
+button.pack(side=tk.LEFT)
+
+frame.place(x=10, y=20)
 root.mainloop()
