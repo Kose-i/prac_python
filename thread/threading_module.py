b465e2895d5ee7e373b79cb7fb0e36092a65ac32
Kose-i <tamura.kosei905@mail.kyutech.jp>
2019-03-07 17:49:22 +0900

    Add threading dir

--- thread/threading_module.py
+++ thread/threading_module.py
@@ -1,4 +1,24 @@
 #! /usr/bin/env python3
 
 import threading
+import time
 
+def thread1_func():
+    # thread の名前を取得
+    print('start 1')
+    time.sleep(5)
+    print('end 1')
+
+def thread2_func():
+    print('start 2')
+    time.sleep(5)
+    print('end 2')
+
+if __name__ == '__main__':
+    # スレッドに workder1 関数を渡す
+    thread1 = threading.Thread(target=thread1_func)
+    thread2 = threading.Thread(target=thread2_func)
+    # スレッドスタート
+    thread1.start()
+    thread2.start()
+    print('started all')
